e99daaa38b3baf5b26aace315d649e129671a3a4
Haley Morgan <haleymorgan3264@gmail.com>
2019-06-06 21:00:43 -0400

    setup variables and constants

---
+++ script.js
@@ -0,0 +1,9 @@
+// Setup variable and constants
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
